b165a7ab9d6dc3f31446d86c12b5ab25d35f9415
Vladimir Ponomarev <vladimir.ponomarev@webpros.com>
2025-06-17 15:15:23 +0300

    Updated amazon at 06/17/25-15:15:23

--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ while true
 do 
     for company in $(bash -c "ls companies/") 
     do 
-        rm -rf companies/$company
-        proxychains4 -q ./asn
-        proxychains4 -q ./asn
+        # rm -rf companies/$company
+        # proxychains4 -q ./asn
+        # proxychains4 -q ./asn
         echo "companies/" > index.html && for i in $(bash -c "ls -l companies/ | cut -d ' ' -f 9"); do echo " - $i/ranges.txt" >> index.html; done
         git add .
         git commit -m "Updated $company at $(date +%D-%T)"
